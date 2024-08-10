8bd5023e2e2419b996641d64b4975807bdf74c84
trba1810 <trba8l@live.com>
2024-08-11 00:51:46 +0200

    added test controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
-    // #[Route('/')]
-    // public function homepage()
-    // {
-    //     return new Response('test');
-    // }
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        $testarray = [
+            [
+                'name' => 'pera',
+                'prezime' => 'peric'
+            ],
+            [
+                'name' => 'zika',
+                'prezime' => 'zikic'
+            ]
+        ];
+        return $this->json($testarray);
+    }
 }
