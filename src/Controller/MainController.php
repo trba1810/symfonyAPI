7198f0d613dc0b80b607c2ce346c0a508c97d79b
trba1810 <trba8l@live.com>
2024-08-17 21:57:37 +0200

    make migration using doctrine

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Project;
+use App\Entity\Projects;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,9 +12,34 @@ class MainController extends AbstractController
     #[Route('/')]
     public function homepage(): Response
     {
-        $testarray = [new Project(
-            'projekat 1',
-        ), new Project('projekat 2')];
-        return $this->json($testarray);
+//        // Creating instances and storing them in an array
+//        $array = [];
+//
+//// Assuming today's date for simplicity
+//        $today = new \DateTimeImmutable();
+//
+//// Creating three projects
+//        $project1 = new Projects();
+//        $project1->setName('Project One');
+//        $project1->setStartedAt($today);
+//        $project1->setTotalHours(10);
+//
+//        $project2 = new Projects();
+//        $project2->setName('Project Two');
+//        $project2->setStartedAt($today);
+//        $project2->setTotalHours(20);
+//
+//        $project3 = new Projects();
+//        $project3->setName('Project Three');
+//        $project3->setStartedAt($today);
+//        $project3->setTotalHours(30);
+//
+//// Adding projects to the array
+//        $array[] = $project1;
+//        $array[] = $project2;
+//        $array[] = $project3;
+//
+//// At this point, $array contains three instances of Projects
+        return $this->json('$array');
     }
 }
