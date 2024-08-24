bcd790ed8d5e9c25b6e7607f34eddc4a70d21fd1
trba1810 <trba8l@live.com>
2024-08-24 18:24:33 +0200

    make show and create from database

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,43 +3,39 @@
 namespace App\Controller;
 
 use App\Entity\Projects;
+use App\Repository\ProjectsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use DateTimeImmutable;
+
 
 class MainController extends AbstractController
 {
-    #[Route('/')]
-    public function homepage(): Response
+    #[Route('/projects', name: 'projects')]
+    public function projects(ProjectsRepository $projectsRepository): Response
     {
-//        // Creating instances and storing them in an array
-//        $array = [];
-//
-//// Assuming today's date for simplicity
-//        $today = new \DateTimeImmutable();
-//
-//// Creating three projects
-//        $project1 = new Projects();
-//        $project1->setName('Project One');
-//        $project1->setStartedAt($today);
-//        $project1->setTotalHours(10);
-//
-//        $project2 = new Projects();
-//        $project2->setName('Project Two');
-//        $project2->setStartedAt($today);
-//        $project2->setTotalHours(20);
-//
-//        $project3 = new Projects();
-//        $project3->setName('Project Three');
-//        $project3->setStartedAt($today);
-//        $project3->setTotalHours(30);
-//
-//// Adding projects to the array
-//        $array[] = $project1;
-//        $array[] = $project2;
-//        $array[] = $project3;
-//
-//// At this point, $array contains three instances of Projects
-        return $this->json('$array');
+        $projects = $projectsRepository->findAll();
+
+        return $this->json($projects);
+    }
+
+    #[Route('/projects/create', name: 'create_project', methods: ['GET', 'POST'])]
+    public function createProject(EntityManagerInterface $entityManager): Response
+    {
+        $project = new Projects();
+        $project->setName('Test1');
+        $startedAt = new DateTimeImmutable();
+        $project->setStartedAt($startedAt);
+        $project->setTotalHours(3);
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($project);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id ' . $project->getId());
     }
 }
